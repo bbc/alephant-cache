7ecd3f3bd7f705855dffe67ce6afba92c7272a03
Integralist <mark.mcdx@gmail.com>
2015-06-17 14:38:05 +0100

    Remove json dependencies

--- lib/alephant/cache.rb
+++ lib/alephant/cache.rb
@@ -1,10 +1,6 @@
 require 'alephant/cache/version'
 require 'alephant/logger'
-require 'alephant/logger/json'
 require 'aws-sdk'
-
-json_driver = Alephant::Logger::JSON.new(ENV["APP_LOG_LOCATION"] ||= "app.log")
-Alephant::Logger.setup json_driver
 
 module Alephant
   class Cache
